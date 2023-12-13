46bf0231f9234e313f989206ff4a03940b2d8b8f
Eugene Kroshinski <137826689+EugeneKroshinsky@users.noreply.github.com>
2023-12-13 13:35:30 +0300

    Update INDIVIDUALLabyrinth.cpp

--- data structures/INDIVIDUALLabyrinth.cpp
+++ data structures/INDIVIDUALLabyrinth.cpp
@@ -1,1 +1,43 @@
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("input.txt"));
+        PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
+
+        int m, c, n;
+        m = in.nextInt();
+        c = in.nextInt();
+        n = in.nextInt();
+
+        int[] hashTable = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            hashTable[i] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            int j = 0;
+            while (true){
+                int currentPos = ((x % m) + c * j) % m;
+                if(hashTable[currentPos] == -1 || hashTable[currentPos] == x){
+                    hashTable[currentPos] = x;
+                    break;
+                }
+                j++;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            out.print(hashTable[i] + " ");
+        }
+        out.flush();
+    }
+}
